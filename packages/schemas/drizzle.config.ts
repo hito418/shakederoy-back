571bfe96cf6bed09c4b1475f8a7c8fd7550826e3
rokin418 <rokinssb@gmail.com>
2026-01-16 14:26:28 +0100

    feat: use env for migrations

--- packages/schemas/drizzle.config.ts
+++ packages/schemas/drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   schema: './src/schemas/**/*',
   out: './migrations',
   dbCredentials: {
-    url: 'postgresql://postgres:postgres@postgres:5432/postgres',
+    url: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.POSTGRES_DB}`,
   },
 })
