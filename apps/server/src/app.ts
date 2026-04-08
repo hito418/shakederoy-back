327901eb9cda723599d2002c24e823769600977e
hito418 <hito.wt@gmail.com>
2026-04-08 10:32:00 +0200

    test: dsn bugsink

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -33,5 +33,6 @@ export const app = new Hono()
   })
   .onError((err, ctx) => {
     Sentry.captureException(err)
+    console.log('err', err)
     return ctx.json({ error: 'Internal Server Error' }, 500)
   })
--- apps/server/src/instrument.ts
+++ apps/server/src/instrument.ts
@@ -1,10 +1,14 @@
 import * as Sentry from '@sentry/node'
 import { env } from './shared/env'
 
+console.log('DSN', env.SENTRY_DSN)
+
 Sentry.init({
   dsn: env.SENTRY_DSN,
   environment: env.NODE_ENV.toLowerCase(),
   enabled: env.NODE_ENV !== 'DEV',
   sendDefaultPii: true,
   tracesSampleRate: env.NODE_ENV === 'PROD' ? 0.2 : 1.0,
+  integrations: [],
+  release: 'shakederoy@1.0.0',
 })
